6df99bb868cdbda22f368def7602f89aaaacb5d7
snownico0722 <36026283+snownico0722@users.noreply.github.com>
2026-09-14 18:47:31 +0800

    feat(settings): surface note background in visual settings

--- src/AppController.SettingsSidebar.cs
+++ src/AppController.SettingsSidebar.cs
@@ -415,7 +415,7 @@ public sealed partial class AppController
         SettingsPage.General => BuildSettingsSidebarGeneralPage(),
         SettingsPage.Todo => BuildSettingsSidebarTodoPage(),
         SettingsPage.Note => BuildSettingsSidebarNotePage(),
-        SettingsPage.Visual => BuildVisualSettingsPage(),
+        SettingsPage.Visual => BuildVisualSettingsPageWithNoteBackground(),
         SettingsPage.Shortcuts => BuildShortcutSettingsPage(),
         SettingsPage.Plugins => BuildPluginsSettingsPage(),
         SettingsPage.Labs => BuildLabsSettingsPage(),
